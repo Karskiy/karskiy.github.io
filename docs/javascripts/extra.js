ad550e86efd172756ba09568d8d84a3e09a6d7c4
Karskiy <karskiy@yahoo.com>
2026-01-01 17:58:18 +0300

    feat: add logo, favicon and custom css/js support

---
+++ docs/javascripts/extra.js
@@ -0,0 +1,7 @@
+/* Custom JavaScript for Karskiy site */
+
+// Use document$ observable for compatibility with instant loading
+document$.subscribe(function() {
+  // Your custom JavaScript code here
+  console.log('Custom JS loaded');
+});
